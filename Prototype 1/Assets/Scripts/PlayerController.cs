48e83bce020262cfa531112530a43f703475c8bf
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-05 03:24:53 +0600

    Turn left/right on button press

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 15.0f;
+    public float turnSpeed = 0.25f;
+    private float turnInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        turnInput = Input.GetAxis("Horizontal");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // Turn
+        turnInput = Input.GetAxis("Horizontal");
+        turn(turnSpeed);
     }
+
+
+
+    // Movement Functions
+    private void turn (float turnSpeed)
+    {
+        transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime * turnSpeed * turnInput);
+    }
+
 }
