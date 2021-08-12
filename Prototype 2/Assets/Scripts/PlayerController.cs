9a6bbd96c4fd7816f7afbb0179ea03f576280d24
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-12 23:52:47 +0600

    Player horizontal movement enabled

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float horizontalInput;
+    [SerializeField] private float playerSpeed = 10;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * playerSpeed);
     }
 }
