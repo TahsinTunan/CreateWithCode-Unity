12ab08be4f5212419cafdf9083c177d71df86d21
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-13 00:01:40 +0600

    Keep player in screen

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     [SerializeField] private float playerSpeed = 10;
+    [SerializeField] private float maxLeft = -15, maxRight = 15;
 
     void Start()
     {
@@ -17,5 +18,20 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * playerSpeed);
+        keepPlayerInBound(maxLeft, maxRight);
+    }
+
+
+    void keepPlayerInBound(float maxLeft, float maxRight)
+    {
+        if (transform.position.x <= maxLeft)
+        {
+            transform.position = new Vector3 (maxLeft, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x >= maxRight)
+        {
+            transform.position = new Vector3 (maxRight, transform.position.y, transform.position.z);
+        }
     }
 }
