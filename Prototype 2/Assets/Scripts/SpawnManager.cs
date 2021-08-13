01b8063a71c615c389cdcfe8f31829715c2fbe63
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-13 22:50:55 +0600

    SpawnManager updated

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,26 +7,34 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private List<GameObject> animalPrefabs;
     [SerializeField] private float xRange = 15;
     [SerializeField] private float zLocation = 20;
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
 
     void Start()
     {
-        
+        InvokeRepeating("InstantiateRandomly", startDelay, spawnInterval);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            InstantiateRandomly(animalPrefabs);
-        }
+        
     }
 
-    void InstantiateRandomly(List<GameObject> prefabs)
+    // void InstantiateRandomly(List<GameObject> prefabs)
+    // {
+    //     int randomIndex = Random.Range(0, prefabs.Count);
+    //     float randomLocationX = Random.Range(-xRange, xRange);
+    //     var spawnLocation = new Vector3 (randomLocationX, 0, zLocation);
+
+    //     Instantiate(prefabs[randomIndex], spawnLocation, prefabs[randomIndex].transform.rotation);
+    // }
+
+    void InstantiateRandomly()
     {
-        int randomIndex = Random.Range(0, prefabs.Count);
+        int randomIndex = Random.Range(0, animalPrefabs.Count);
         float randomLocationX = Random.Range(-xRange, xRange);
         var spawnLocation = new Vector3 (randomLocationX, 0, zLocation);
 
-        Instantiate(prefabs[randomIndex], spawnLocation, prefabs[randomIndex].transform.rotation);
+        Instantiate(animalPrefabs[randomIndex], spawnLocation, animalPrefabs[randomIndex].transform.rotation);
     }
 }
