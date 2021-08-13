a7f0eba5cc1a59b42c2035e7a3fa42308d3a549d
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-13 14:28:28 +0600

    Projectile works

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,18 +7,20 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     [SerializeField] private float playerSpeed = 10;
     [SerializeField] private float xRange = 15;
+    [SerializeField] GameObject projectilePrefab;
 
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * playerSpeed);
+        movePlayer(playerSpeed);
         keepPlayerInBound(-xRange, xRange);
+        shootProjectile(projectilePrefab);
     }
 
 
@@ -26,12 +28,25 @@ public class PlayerController : MonoBehaviour
     {
         if (transform.position.x <= xLeftRange)
         {
-            transform.position = new Vector3 (xLeftRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xLeftRange, transform.position.y, transform.position.z);
         }
 
         if (transform.position.x >= xRightRange)
         {
-            transform.position = new Vector3 (xRightRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRightRange, transform.position.y, transform.position.z);
         }
+    }
+
+    void shootProjectile(GameObject projectile)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+        }
+    }
+
+    void movePlayer(float playerSpeed)
+    {
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * playerSpeed);
     }
 }
--- Prototype 2/Assets/Scripts/ProjectileMoveForward.cs
+++ Prototype 2/Assets/Scripts/ProjectileMoveForward.cs
@@ -8,7 +8,7 @@ public class ProjectileMoveForward : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     
