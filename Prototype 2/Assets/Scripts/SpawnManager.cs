a7b48ef25c13bc88b1b07c5177b3438643d9647b
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-14 00:02:38 +0600

    Coroutine added

--- Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,34 +7,29 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private List<GameObject> animalPrefabs;
     [SerializeField] private float xRange = 15;
     [SerializeField] private float zLocation = 20;
-    private float startDelay = 2f;
+    // private float startDelay = 2f;
     private float spawnInterval = 1.5f;
 
     void Start()
     {
-        InvokeRepeating("InstantiateRandomly", startDelay, spawnInterval);
+        StartCoroutine(InstantiateRandomlyCoroutine(animalPrefabs));
     }
 
-    void Update()
+
+    IEnumerator InstantiateRandomlyCoroutine(List<GameObject> prefabs)
     {
-        
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            InstantiateRandomly(prefabs);
+        }
     }
 
-    // void InstantiateRandomly(List<GameObject> prefabs)
-    // {
-    //     int randomIndex = Random.Range(0, prefabs.Count);
-    //     float randomLocationX = Random.Range(-xRange, xRange);
-    //     var spawnLocation = new Vector3 (randomLocationX, 0, zLocation);
-
-    //     Instantiate(prefabs[randomIndex], spawnLocation, prefabs[randomIndex].transform.rotation);
-    // }
-
-    void InstantiateRandomly()
+    void InstantiateRandomly(List<GameObject> prefabs)
     {
-        int randomIndex = Random.Range(0, animalPrefabs.Count);
+        int randomIndex = Random.Range(0, prefabs.Count);
         float randomLocationX = Random.Range(-xRange, xRange);
-        var spawnLocation = new Vector3 (randomLocationX, 0, zLocation);
-
-        Instantiate(animalPrefabs[randomIndex], spawnLocation, animalPrefabs[randomIndex].transform.rotation);
+        var spawnLocation = new Vector3(randomLocationX, 0, zLocation);
+        Instantiate(prefabs[randomIndex], spawnLocation, prefabs[randomIndex].transform.rotation);
     }
 }
