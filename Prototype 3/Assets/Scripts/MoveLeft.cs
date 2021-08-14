374c1425001ea37dd3d251fa50d9cab853366ccf
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-08-15 01:23:19 +0600

    MoveLeft script

---
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    [SerializeField] private float speed = 10;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * Time.deltaTime * speed);
+    }
+}
