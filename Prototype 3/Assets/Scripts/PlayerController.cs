3596fff1c260299c1f4b9f00f46a637e8f8c530e
TahsinTunan <53114840+TahsinTunan@users.noreply.github.com>
2021-09-29 06:37:57 +0600

    Death Animation fix

--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.Space)) && isGrounded)
+        if ((Input.GetKeyDown(KeyCode.Space)) && isGrounded && !gameOver)
         {
             jump(playerRB, jumpForce);
         }
